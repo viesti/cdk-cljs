cb4385ef20a73b7e925ff0712989deab58892d6c
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2021-09-24 21:38:49 +0300

    Add note on magic

--- src/iac/demo.cljs
+++ src/iac/demo.cljs
@@ -9,6 +9,7 @@
 (defn main []
   (let [app (App.)
         stack (Stack. app)]
+    ;; "Magic" happens here. Bucket is not actually used anywhere, but get's registered to the stack
     (Bucket. stack "my-bucket")
     (let [assembly (.synth app)]
       (println (.write decoder (fs/readFileSync (str (.-directory assembly) "/Default.template.json")))))))
